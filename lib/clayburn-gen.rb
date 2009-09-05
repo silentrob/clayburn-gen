777d97fe2770d5dc63c48efdce00b06de27329b4
Brock Whitten <brock@sintaxi.com>
2009-09-05 03:45:27 -0700

    added git init and clyburn submodule support to app generator

--- lib/clayburn-gen.rb
+++ lib/clayburn-gen.rb
@@ -7,18 +7,38 @@ class Clayburn
   end
   
   def app(name)
-    puts "\nGenerating a new Clayburn Application called #{name}\n\n"
     generate_path = File.join(Dir.pwd,name)    
     template_path = File.join(File.dirname(__FILE__),'templates/clayburn-app')
-    FileUtils.cp_r(template_path, generate_path)
-
-    clayburn_path = File.join(File.dirname(__FILE__),'clayburn')
-
-    clayburn_final_path = File.join(generate_path,'127.0.0.1/js/lib/clayburn')
     
-    # puts "ClayPath #{clayburn_path} Final: #{clayburn_final_path}"
-    FileUtils.cp_r(clayburn_path, clayburn_final_path)      
-
+    if File.exist?(generate_path)
+      puts "error: a file or directory called #{name} already exists."
+    else
+      # copies templates
+      ::FileUtils.cp_r(template_path, generate_path)
+      if File.exist?(generate_path)
+        puts "\nGenerating with clayburn generator:"
+        puts "     [ADDED]  clayburn.sh"
+        puts "     [ADDED]  127.0.0.1/js/"
+        puts "     [ADDED]  127.0.0.1/js/bootstrap.js"
+        puts "     [ADDED]  127.0.0.1/js/config.js"
+        puts "     [ADDED]  127.0.0.1/web/"
+        puts "     [ADDED]  127.0.0.1/web/public/"
+      end
+      
+      # git init
+      if `cd #{generate_path}/127.0.0.1 && git init && git add . && git commit -am "initial commit by clayburn generator"`
+        puts "     [GIT]    init"
+        puts "     [GIT]    add ."
+        puts "     [GIT]    commit -m \"initial commit by clayburn generator\""
+      end
+      
+      # submodule clayburn
+      if `cd #{generate_path}/127.0.0.1 && git submodule add git://github.com/silentrob/clayburn.git js/clayburn && git add . && git commit -am "added clayburn as a git submodule"`
+        puts "     [GIT]    submodule add git://github.com/silentrob/clayburn.git js/clayburn"
+        puts "     [GIT]    add ."
+        puts "     [GIT]    commit -m \"added clayburn as a git submodule\""
+      end
+    end
   end
   
   def flat(name)
